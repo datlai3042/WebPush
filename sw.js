6417d20e224569ee07ac19de0152a0db0e3a28d6
datlai3042 <datlai304@gmail.com>
2024-11-21 16:48:26 +0700

    Test web

--- sw.js
+++ sw.js
@@ -1,3 +1,34 @@
+const title = 'Actions Notification';
+
+const options = {
+  actions: [
+    {
+      action: 'coffee-action',
+      type: 'button',
+      title: 'Coffee',
+      icon: '/images/demos/action-1-128x128.png',
+    },
+    {
+      action: 'doughnut-action',
+      type: 'button',
+      title: 'Doughnut',
+      icon: '/images/demos/action-2-128x128.png',
+    },
+    {
+      action: 'gramophone-action',
+      type: 'button',
+      title: 'Gramophone',
+      icon: '/images/demos/action-3-128x128.png',
+    },
+    {
+      action: 'atom-action',
+      type: 'button',
+      title: 'Atom',
+      icon: '/images/demos/action-4-128x128.png',
+    },
+  ],
+};
+
 self.addEventListener('push', () => {
-  self.registration.showNotification('Hello world!', options);
+  self.registration.showNotification(title, options);
 });
--- webpush.js
+++ webpush.js
@@ -4,9 +4,8 @@ const webpush = require("web-push");
 // console.log({ result });
 // VAPID keys should only be generated only once.
 const vapidKeys = {
-  publicKey:
-    "BPazQquhXWBJuKfzo3Tz_h9nhSYnb2A9KRC3LgDrPl4giz-OmLg3SjrlDkc_eoAocW3Ty204LBEq5Z5_5SF79Ik",
-  privateKey: "c2ktsUElSe189lexuqbeYpWME2_KU23aZpf85iBQy70",
+ publicKey: 'BLGT3uloHwB_V6ozWLHJuux87VLnH5mlkgWNNqS1ZMNanGISbJWiMYkxYNopmC5Sv-aWljcSrjflaGpl6PMRTAY',
+    privateKey: '8vzv0g3MH37mOPkP-jf4bMdnr2TncmGYOSoJtInJtbU'
 };
 
 webpush.setVapidDetails(
@@ -18,12 +17,23 @@ webpush.setVapidDetails(
 // This is the same output of calling JSON.stringify on a PushSubscription
 const pushSubscription = {
   endpoint:
-    "https://fcm.googleapis.com/fcm/send/djvZlVXKe9A:APA91bFfJc2fJo33bo6_kR0PKlc2yFA3B9yz2XlW4i9htG4tKuuHJ0_0PDrObLXo9Vbm0syBa_o-L8OXhrMiW0ROs93KRUHmQovIDSlT_EDKKEats3-oRpbEFTOvLQZ5uz8Rl6cXg8Vf",
+    "https://fcm.googleapis.com/fcm/send/fPuOmrAJ51Q:APA91bH_ktJHhB6vZnVwGLdUG6t3g6AlS3IE2droH8n_6gJo6EDvC9bIxp2kU99YqZ4XmYhZr3tl5fF1AqXfMk_5pET2aEmQuioG08P-ZFdpcZz89v7G09uMyVhzXVXSwQa2wPwYkFtU",
   expirationTime: null,
   keys: {
     p256dh:
-      "BC-MfglhBpT53fCom5DNzdVjlNmXLnXqAK7Qf-xpKG-IYkD94miTLtnMpwHZ1A2uM8ccajGk81FC3IGVrbrJkj8",
-    auth: "DKbidcGsr-anyN_Iy8qK0Q",
+      "BBFsy_WCbmsRVafani-Ktkium9MZUehvhxOVRCKnK87hP33gK_Z1qHPT_sl5vT5s-3XmNSTbt_Xp2w-RkBK0Fhg",
+    auth: "3vTWyvpffnLdqZq8z9C4PA",
+  },
+};
+
+const v2 = {
+  endpoint:
+    "https://fcm.googleapis.com/fcm/send/fPuOmrAJ51Q:APA91bH_ktJHhB6vZnVwGLdUG6t3g6AlS3IE2droH8n_6gJo6EDvC9bIxp2kU99YqZ4XmYhZr3tl5fF1AqXfMk_5pET2aEmQuioG08P-ZFdpcZz89v7G09uMyVhzXVXSwQa2wPwYkFtU",
+  expirationTime: null,
+  keys: {
+    p256dh:
+      "BBFsy_WCbmsRVafani-Ktkium9MZUehvhxOVRCKnK87hP33gK_Z1qHPT_sl5vT5s-3XmNSTbt_Xp2w-RkBK0Fhg",
+    auth: "3vTWyvpffnLdqZq8z9C4PA",
   },
 };
 

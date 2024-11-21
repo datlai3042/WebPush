594ed73e8fbfbe9b8866299202f7461d2cee7ce8
datlai3042 <datlai304@gmail.com>
2024-11-21 16:31:03 +0700

    Sửa token

--- webpush.js
+++ webpush.js
@@ -1,29 +1,30 @@
-const webpush = require('web-push');
+const webpush = require("web-push");
 
-
-let result = webpush.generateVAPIDKeys()
-console.log({result})
+// let result = webpush.generateVAPIDKeys();
+// console.log({ result });
 // VAPID keys should only be generated only once.
 const vapidKeys = {
-publicKey: 'BAJw6OLzvPeND1T6ZcU5CmCV8GMq5JxxrSqGwxuFAfVU86HL9xw31f-nCTHuF_8qOtYswbqMYJfKiJlFbEzAC34',
-  privateKey: '5C60gkItEHcn9paB6SYWf2fri1_bZN2xXGZVzasc-Pw'
- };
+  publicKey:
+    "BPazQquhXWBJuKfzo3Tz_h9nhSYnb2A9KRC3LgDrPl4giz-OmLg3SjrlDkc_eoAocW3Ty204LBEq5Z5_5SF79Ik",
+  privateKey: "c2ktsUElSe189lexuqbeYpWME2_KU23aZpf85iBQy70",
+};
 
 webpush.setVapidDetails(
-  'mailto:example@yourdomain.org',
+  "mailto:example@yourdomain.org",
   vapidKeys.publicKey,
   vapidKeys.privateKey
 );
 
 // This is the same output of calling JSON.stringify on a PushSubscription
 const pushSubscription = {
-  endpoint: '.....',
+  endpoint:
+    "https://fcm.googleapis.com/fcm/send/djvZlVXKe9A:APA91bFfJc2fJo33bo6_kR0PKlc2yFA3B9yz2XlW4i9htG4tKuuHJ0_0PDrObLXo9Vbm0syBa_o-L8OXhrMiW0ROs93KRUHmQovIDSlT_EDKKEats3-oRpbEFTOvLQZ5uz8Rl6cXg8Vf",
+  expirationTime: null,
   keys: {
-    auth: '.....',
-    p256dh: '.....'
-  }
+    p256dh:
+      "BC-MfglhBpT53fCom5DNzdVjlNmXLnXqAK7Qf-xpKG-IYkD94miTLtnMpwHZ1A2uM8ccajGk81FC3IGVrbrJkj8",
+    auth: "DKbidcGsr-anyN_Iy8qK0Q",
+  },
 };
 
-webpush.sendNotification(pushSubscription, 'Your Push Payload Text');
-
-
+webpush.sendNotification(pushSubscription, "Your Push Payload Text");
